f1d2ab03d43995051fd2351a734e6c894194f64e
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2022-11-08 11:55:33 -0400

    aula

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-
+# flask
 DEBUG = True
 
 #Configs banco de dados
-USERNAME = ''
+USERNAME = 'root'
 PASSWORD = ''
-SERVER = ''
-DATABASE = ''
+SERVER = 'localhost'
+DATABASE = 'sistema_controle_estagiario'
 
 SQLALCHEMY_DATABASE_URI = f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}'
